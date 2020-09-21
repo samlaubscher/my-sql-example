972bcc2b4acb8b1a303e73c8de715cd6054ad494
Sam Laubscher <hakaewt@protonmail.com>
2020-09-21 16:45:00 +0000

    Add Friends

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -11,7 +11,11 @@ connection = pymysql.connect(host='localhost',
 
 try:
     with connection.cursor() as cursor:
-        cursor.execute("""create table if not exists
-                        Friends(name char(20), age int, DOB datetime);""")
+        rows = [("Bob", 21, "1990-02-06 23:12:03"),
+                ("Jim", 56, "1993-03-09 21:11:13"),
+                ("Tim", 31, "1970-02-06 23:12:03"),
+                ("Matt", 51, "1980-02-06 23:12:03")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s, %s, %s);", rows)
+        connection.commit()
 finally:
     connection.close()
